1dbb770440b70b531446b7576e450d8a70bae1fe
Luna Amora <luna.mberry@gmail.com>
2022-08-20 17:42:21 -0400

    Add exploding obstacles

---
+++ Assets/Scripts/ExplodingObject.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace LyeJam
+{
+    public class ExplodingObject : MonoBehaviour
+    {
+        [SerializeField] private GameObject _parts;
+        [SerializeField] private Renderer _renderer;
+        [SerializeField] private Collider _collider;
+
+
+        void OnCollisionEnter(Collision hit)
+        {
+            if(hit.transform.gameObject.tag == "Player")
+            {
+                var sphere = hit.transform.gameObject.GetComponent<PlayerSphere>();
+
+                if(!sphere.IsGhost)
+                {
+                    _renderer.enabled = false;
+                    _collider.enabled = false;
+                    _parts.SetActive(true);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerSphere.cs
+++ Assets/Scripts/PlayerSphere.cs
@@ -5,9 +5,12 @@ namespace LyeJam
     public class PlayerSphere : MonoBehaviour
     {
         [SerializeField] private Rigidbody _rigidBody;
+        
+        public bool IsGhost = false;
 
         public void Init(Vector3 velocity, bool isGhost = false)
         {
+            IsGhost = isGhost;
             _rigidBody.velocity = Vector3.zero;
             _rigidBody.AddForce(velocity, ForceMode.Impulse);
         }
