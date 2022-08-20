6b34e747c80644d060c2dc28087f2220af5c31a5
Luna Amora <luna.mberry@gmail.com>
2022-08-20 15:19:56 -0400

    Add `Dona Jura`

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,12 @@ namespace LyeJam
     {
         [SerializeField] private InputReader _input;
 
+        private bool isPaused = false;
+
         void Start()
         {
             _input.OnResetEvent += ResetScene;
+            _input.OnPauseEvent += PauseGame;
         }
 
         void OnDestroy()
@@ -20,6 +23,20 @@ namespace LyeJam
         void ResetScene()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        void PauseGame()
+        {
+            if(!isPaused)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+            
+            isPaused = !isPaused;
         }
     }
 }
