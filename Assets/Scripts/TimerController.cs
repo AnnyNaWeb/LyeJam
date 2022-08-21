c654625c17aa92fdb2271e9b0630761aed8890bf
InaGameStudio <developer@inagamestudio.com>
2022-08-20 22:39:03 -0300

    Teste

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LyeJam
+{
+    public class TimerController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
