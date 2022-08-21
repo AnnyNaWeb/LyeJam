314a662a21d521360c04be589204041f986d1eb5
InaGameStudio <developer@inagamestudio.com>
2022-08-20 23:08:16 -0300

    Soma Tempo atualizado

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -9,8 +9,15 @@ namespace LyeJam
         [SerializeField] private Button _play_Button;
         [SerializeField] private Button _exit_Button;
 
+        [SerializeField] public float totalTime;
+        [SerializeField] public Text tempo;
+
+        private float minutes;
+        private static float seconds = 60;
+
         void Start()
         {
+            Text timer = gameObject.GetComponent<Timer>();
             _play_Button.onClick.AddListener(OnPlay);
             _exit_Button.onClick.AddListener(OnExit);
         }
@@ -26,6 +33,19 @@ namespace LyeJam
             UnityEditor.EditorApplication.isPlaying = false;
 #endif
             Application.Quit();
+        }
+
+        void PlayTimer()
+        {
+            totalTime += Time.deltaTime;
+            minutes = (int)(totalTime / 60);
+            seconds = (int)(totalTime % 60);
+            tempo.text = minutes.ToString() + " : " + seconds.ToString();
+        }
+
+        void Update()
+        {
+            PlayTimer();
         }
     }
 }
