dbe98beee26faeb5e4e7b515ca735a0dde8d87a3
Luna Amora <luna.mberry@gmail.com>
2022-08-21 16:35:39 -0400

    Fix computer assets

--- Assets/Scripts/ExplodingObject.cs
+++ Assets/Scripts/ExplodingObject.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace LyeJam
@@ -8,13 +9,10 @@ namespace LyeJam
         [SerializeField] private Renderer _renderer;
         [SerializeField] private Collider _collider;
 
-
         void OnCollisionEnter(Collision hit)
         {
-            
             if(hit.transform.gameObject.tag == "Player")
             {
-                
                 var sphere = hit.transform.gameObject.GetComponent<PlayerSphere>();
 
                 if(!sphere.IsGhost)
