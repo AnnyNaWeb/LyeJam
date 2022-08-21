a9e25b9529ad749d0c09bbc7cb84a251e21494ab
InaGameStudio <developer@inagamestudio.com>
2022-08-20 22:53:32 -0300

    Soma Tempo e Exibe funcionando

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LyeJam
 {
     public class TimerController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public float totalTime;
+        public Text tempo;
+
+        private float minutes;
+        private static float seconds = 60;
+        void PlayLevel ()
         {
-        
+            totalTime += Time.deltaTime;
+            minutes = (int)(totalTime / 60);
+            seconds = (int)(totalTime % 60);
+            tempo.text = minutes.ToString() + " : " + seconds.ToString();
         }
 
         
         void Update()
         {
-        
+            PlayLevel();
         }
     }
 }
